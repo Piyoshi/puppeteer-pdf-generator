207613b52ce88f471f56b9157b1529ecf4402702
Satoshi Ueno <satop@palette.cloud>
2020-05-11 13:06:20 +0900

    fix dev's pdf-generate script for apply command line argments option

--- scripts/dev/pdf-generate.js
+++ scripts/dev/pdf-generate.js
@@ -1,11 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
-const html = fs.readFileSync(path.resolve(path.join('views', 'sample.html')), "utf-8");
+const { program } = require('commander');
+
+program.option('-f, --file <file>', 'target view html file', 'sample.html').parse(process.argv);
+console.log("arguments option: ", program.opts());
+const filename = program.file;
+const html = fs.readFileSync(path.resolve(path.join('views', program.file)), "utf-8");
 
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
+  await page.emulateMediaType('screen')
   await page.setContent(html);
   await page.pdf({ path: path.resolve(path.join('tmp', 'sample.pdf')), format: 'A4' });
   await browser.close();
